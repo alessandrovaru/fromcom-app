cb9e898f1fd355eefbe42f92905be760cbaa6c0d
Alessandro <alessandrovaru@gmail.com>
2023-11-11 12:25:20 -0400

    Update background color and add animations to description section

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,19 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.logoAnimationContainer}>
-        Video
+       <Image className='img-fluid' src='/logos/fdo1.gif' width={1280} height={720}/>
       </div>
       <div className={styles.descriptionContainer}>
         <h2>We create content for the largest minority:</h2>
         <h3>WOMEN</h3>
       </div>
       <div className={styles.email}>
-        <a href='#'>hello@thefromcompictures.com</a>
+        <a href='mailto:hello@thefromcompictures.com'><p>hello@thefromcompictures.com</p></a>
       </div>
     </main>
   )
