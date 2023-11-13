82c977d3dc1231e6d310a6895421065cdd197eed
Alessandro <alessandrovaru@gmail.com>
2023-11-13 12:39:12 -0400

    Add reveal animation to description and email sections

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,14 @@ export default function Home() {
       <div className={styles.logoAnimationContainer}>
        <Image className='img-fluid' src='/logos/fdo1.gif' width={1280} height={720}/>
       </div>
-      <div className={styles.descriptionContainer}>
-        <h2>We create content for the largest minority:</h2>
-        <h3>WOMEN</h3>
-      </div>
-      <div className={styles.email}>
-        <a href='mailto:hello@thefromcompictures.com'><p>hello@thefromcompictures.com</p></a>
+      <div className={styles.revealContainer}>
+        <div className={styles.descriptionContainer}>
+          <h2>We create content for the largest minority:</h2>
+          <h3>WOMEN</h3>
+        </div>
+        <div className={styles.email}>
+          <a href='mailto:hello@thefromcompictures.com'><p>hello@thefromcompictures.com</p></a>
+        </div>
       </div>
     </main>
   )
